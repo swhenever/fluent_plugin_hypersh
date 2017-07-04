862c2140ee58294c6c376c3ab917897b5c84a697
Jesse Mortenson <jessemortenson@gmail.com>
2017-07-03 20:14:16 -0500

    Initial commit

---
+++ in_hypersh.rb
@@ -0,0 +1,60 @@
+require 'fluent/plugin/input'
+require 'json'
+
+module Fluent::Plugin
+  class HypershInput < Input
+    # First, register the plugin. NAME is the name of this plugin
+    # and identifies the plugin in the configuration file.
+    Fluent::Plugin.register_input('hypersh', self)
+
+    # config_param defines a parameter. You can refer a parameter via @port instance variable
+    # :default means this parameter is optional
+    config_param :service, :string
+
+    # This method is called before starting.
+    # 'conf' is a Hash that includes configuration parameters.
+    # If the configuration is invalid, raise Fluent::ConfigError.
+    def configure(conf)
+      super
+
+      # configured "port" is referred by `@port` or instance method #port
+      if @service == ''
+        raise Fluent::ConfigError, "service parameter must be defined"
+      end
+    end
+
+    # This method is called when starting.
+    # Open sockets or files and create a thread here.
+    def start
+      super
+
+      # Identify containers running under the requested service
+      service_info = JSON.parse(`hyper service inspect #{service}`)
+
+      tag = "hypersh.#{service}"
+
+      # start thread for each container
+      # service_info[0]["Containers"][0]
+      service_info[0]["Containers"].each do |container_id|
+        t = Thread.new { 
+          IO.popen("hyper logs --since='1s' -t -f #{container_id}") do |io|
+            while (line = io.gets) do
+              time = Fluent::Engine.now # TODO parse timestamp from line
+              record = {"message"=>line}
+              router.emit(tag, time, record)
+            end
+          end
+        }
+        t.join
+      end
+      
+    end
+
+    # This method is called when shutting down.
+    def shutdown
+      # my own shutdown code
+
+      super
+    end
+  end
+end
